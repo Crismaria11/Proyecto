9e6a199a639e540b71242c278e349356a96757db
Cristina B <bau161260@uvg.edu.gt>
2018-09-17 19:11:15 -0600

    cambios

---
+++ src/asiento.java
@@ -0,0 +1,13 @@
+public class asiento {
+    String row;
+    int num;
+
+    public void set(String row, int num) {
+        this.row = row;
+        this.num = num;
+    }
+
+    public asiento(String row, int num) {
+        this.set(row, num);
+    }
+}
